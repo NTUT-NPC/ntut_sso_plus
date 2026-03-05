a18e375096b9f7cc9885805feb43745dcf2141ef
Kevin <cgshyuankai@proton.me>
2026-03-06 00:53:20 +0800

    feat: Add NTUT iSchool Downloader card and button to the popup.

--- popup.js
+++ popup.js
@@ -53,7 +53,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const btnConfigs = [
         { id: 'open-course-1-btn', apOu: 'aa_030_oauth' },
         { id: 'open-course-2-btn', apOu: 'aa_030_2_oauth' },
-        { id: 'open-course-3-btn', apOu: 'aa_030_3_oauth' }
+        { id: 'open-course-3-btn', apOu: 'aa_030_3_oauth' },
+        { id: 'open-ischool-btn', apOu: 'ischool_plus_oauth' }
     ];
 
     btnConfigs.forEach(config => {
