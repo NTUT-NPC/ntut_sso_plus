82f953c7b6886edca7b86aa45f2ed6c40cb7d492
kevinlee-06 <cgshyuankai@proton.me>
2026-03-21 10:52:56 +0800

    feat: add portal link and icon to header for easy access to campus website

--- entrypoints/popup/components/Header.vue
+++ entrypoints/popup/components/Header.vue
@@ -33,6 +33,9 @@ const handleLogout = () => {
       <div class="npc-tag">v{{ browser.runtime.getManifest().version }} BY NPC</div>
     </a>
     <div class="header-actions">
+      <a class="icon-btn" href="https://nportal.ntut.edu.tw" target="_blank" title="校園入口網站">
+        <div class="icon portal"></div>
+      </a>
       <a class="icon-btn" href="https://github.com/NTUT-NPC/ntut_sso_plus" target="_blank" title="GitHub 專案">
         <div class="icon github"></div>
       </a>
@@ -122,19 +125,23 @@ header {
   -webkit-mask-position: center;
 }
 
+.portal {
+  mask-image: url('/icons/school.svg');
+  -webkit-mask-image: url('/icons/school.svg');
+}
 .github {
   mask-image: url('/icons/github.svg');
   -webkit-mask-image: url('/icons/github.svg');
 }
 
 .external-link {
-  mask-image: url('/icons/external-link.svg');
-  -webkit-mask-image: url('/icons/external-link.svg');
+  mask-image: url('/icons/open_in_browser.svg');
+  -webkit-mask-image: url('/icons/open_in_browser.svg');
 }
 
 .maximize {
-  mask-image: url('/icons/maximize.svg');
-  -webkit-mask-image: url('/icons/maximize.svg');
+  mask-image: url('/icons/open_in_new_down.svg');
+  -webkit-mask-image: url('/icons/open_in_new_down.svg');
 }
 
 .modern-btn.sm {
